877f56c5677823dec4728f7d550318fe1c465cac
Chris York <christopherayork1992@gmail.com>
2019-08-19 18:52:28 -0400

    MVP Done v2

--- webtesting-iii-challenge/src/dashboard/Dashboard.spec.js
+++ webtesting-iii-challenge/src/dashboard/Dashboard.spec.js
@@ -2,7 +2,15 @@
 
 import React from 'react';
 import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+  it('Should match snapshot', () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
 
 test('Renders display and controls', () => {
   const { getByText } = render(<Dashboard />);
